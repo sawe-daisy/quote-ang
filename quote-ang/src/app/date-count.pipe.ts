80fff58920540aaa36ca4b690ce6b582ce1c5b82
sawe-daisy <deesawe16@gmail.com>
2020-09-28 00:16:09 +0300

    pipe information

--- quote-ang/src/app/date-count.pipe.ts
+++ quote-ang/src/app/date-count.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: any): number {
     return null;
   }
 
