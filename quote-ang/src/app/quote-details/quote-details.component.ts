530036857f92a999b51fbe79ab593d3ef4693a06
sawe-daisy <deesawe16@gmail.com>
2020-09-27 23:47:21 +0300

    cards

--- quote-ang/src/app/quote-details/quote-details.component.ts
+++ quote-ang/src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+@Input() quote: Quote;
 
   constructor() { }
 
